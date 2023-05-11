de83f541c26a2938ab78811aa9de17eb7e4c9879
Roman23223 <roma_chepiga@mail.ru>
2023-05-11 15:50:36 +0300

    dd

--- DjangoProfmaket/settings.py
+++ DjangoProfmaket/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-03c^_mj42g#*q174v6d=4!f%^78x324gtkzwrw_3fniqv0d@o$'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
