04fde7f7f8d80369074d4e6cceb104c07ef11578
Roman23223 <roma_chepiga@mail.ru>
2023-05-18 14:47:31 +0300

    add class ModelTest(APITestCase)

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,46 @@
-from django.test import TestCase
-from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APISimpleTestCase
+from .models import Work, Image, Address, Email, Telephone_number, Application
+
+
+class ModelTest(APITestCase):
+
+    def test_model_work(self):
+        self.work = Work.objects.create(
+            title='тест',
+            content='тест',
+        )
+
+    def test_model_image(self):
+        self.work = Image.objects.create(
+            title='тест',
+        )
+
+    def test_get_list_address(self):
+        self.work = Address.objects.create(
+            address='тест',
+        )
+
+    def test_get_list_email(self):
+        self.work = Email.objects.create(
+            email='test@mail.ru',
+            title='тест',
+        )
+
+    def test_get_list_telephone(self):
+        self.work = Telephone_number.objects.create(
+            number='тест',
+        )
+
+    def test_get_list_application(self):
+        self.work = Application.objects.create(
+            size="тест",
+            product_time="тест",
+            sender_name="тест",
+            number="тест",
+            email="test@mail.ru",
+            comment="тест"
+        )
 
 
 class ViewsTest(APITestCase):
